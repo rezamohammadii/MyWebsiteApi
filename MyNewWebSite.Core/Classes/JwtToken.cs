78c723f671504ab1ffabf6d3267a1a23279f97ff
mr-mohammadi <mr-mohammadi@amnpardaz.ir>
2023-12-24 21:56:11 +0330

    cleaner

--- MyNewWebSite.Core/Classes/JwtToken.cs
+++ MyNewWebSite.Core/Classes/JwtToken.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
-using MyNewWebSite.AccessLayer.Entity;
 
 namespace MyNewWebSite.Core.Classes
 {
@@ -17,23 +16,23 @@ namespace MyNewWebSite.Core.Classes
         {
             _config = config;
         }
-        private string GenerateJSONWebToken(User userModel)
-        {
-            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("Jwt").Value));
-            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
-                _config["Jwt:Issuer"], null, expires: DateTime.Now.AddMinutes(1), signingCredentials: credentials);
-            return new JwtSecurityTokenHandler().WriteToken(token);
-        }
-        private UserModel AuthenticateUser(UserModel login)
-        {
-            UserModel? user = null;
-            if (login.Username == "reza")
-            {
-                user = new UserModel { Username = "reza mohammadi", Email = "reza@gmail.com" };
+        //private string GenerateJSONWebToken(User userModel)
+        //{
+        //    var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("Jwt").Value));
+        //    var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+        //    var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+        //        _config["Jwt:Issuer"], null, expires: DateTime.Now.AddMinutes(1), signingCredentials: credentials);
+        //    return new JwtSecurityTokenHandler().WriteToken(token);
+        //}
+        //private UserModel AuthenticateUser(UserModel login)
+        //{
+        //    UserModel? user = null;
+        //    if (login.Username == "reza")
+        //    {
+        //        user = new UserModel { Username = "reza mohammadi", Email = "reza@gmail.com" };
 
-            }
-            return user;
-        }
+        //    }
+        //    return user;
+        //}
     }
 }
